fd9bfdf987eebf968427f1a830539b7314296f2a
Amin Saberi <a.saberi@head1.htc.inm7.de>
2021-10-06 18:19:52 +0200

    Fix error in helpers.download function

--- code/1_load_data/helpers.py
+++ code/1_load_data/helpers.py
@@ -3,11 +3,11 @@ import shutil
 from urllib.parse import urlparse
 import os
 
-def download(url, out_file=None):
+def download(url, file_name=None):
 	"""
 	Download the file from `url` and save it locally under `file_name`
 	"""
-	if not out_file:
-		out_file = os.path.basename(urlparse(url).path)
+	if not file_name:
+		file_name = os.path.basename(urlparse(url).path)
 	with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
 	    shutil.copyfileobj(response, out_file)
